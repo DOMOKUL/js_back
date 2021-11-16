b98784ef502072356ff3f01e78d1738e687521d4
pokal <strokachnik@gmail.com>
2021-11-16 21:36:29 +1000

    Add token and user models

---
+++ src/dataBase/models/user.model.js
@@ -0,0 +1,49 @@
+const Sequelize = require("sequelize");
+const { sequelize } = require("../index.js");
+const ToDo = require("./todo.model");
+const Token = require("./token.model");
+
+class User extends Sequelize.Model {}
+
+User.init({
+    id: {
+        type: Sequelize.UUIDV4,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+    },
+    login: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+}, {
+    sequelize: sequelize,
+    underscored: true,
+    modelName: "user",
+    timestamps: false,
+    freezeTableName: true
+});
+
+User.hasMany(ToDo);
+User.hasMany(Token);
+
+Token.belongsTo(User, {
+    foreignKey: "userId",
+});
+
+ToDo.belongsTo(User, {
+    foreignKey: "userId",
+});
+
+module.exports = User;
